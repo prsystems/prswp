f4308a749188415e527d658616d488df19ddcb29
Paul Reilly <paul@prsystems.co.uk>
2019-09-16 20:25:03 +0100

    Added Bootstrap formatting to standard pages

--- footer.php
+++ footer.php
@@ -13,21 +13,23 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'prswp' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'prswp' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'prswp' ), 'prswp', '<a href="https://www.prsystems.co.uk">P R Systems</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <footer id="colophon" class="site-footer">
+        <div class="container">
+            <div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'prswp' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'prswp' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> | </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'prswp' ), 'prswp', '<a href="https://www.prsystems.co.uk">P R Systems</a>' );
+					?>
+            </div><!-- .site-info -->
+        </div><!-- .container -->
+    </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,10 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+        <div class="col-12 col-md-4">
+            <aside id="secondary" class="widget-area">
+                <?php dynamic_sidebar( 'sidebar-1' ); ?>
+            </aside><!-- #secondary -->
+        </div><!-- .col-12 .col-md-4-->
+    </div><!-- .row -->
+</div><!-- .container -->
