e2cd95505db02722f88b337eb5b57c51391686db
Paul Reilly <paul@prsystems.co.uk>
2019-09-26 09:20:38 +0100

    Added Full Width & No Title Templates

---
+++ no-title.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Template Name: No Title
+ * @package prstheme
+ */
+
+get_header(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-md-8">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+
+					<?php
+					while ( have_posts() ) :
+						the_post();
+
+						get_template_part( 'template-parts/content', 'no-title' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+
+					endwhile; // End of the loop.
+					?>
+
+                </main><!-- #main -->
+            </div><!-- #primary -->
+        </div><!-- .col-12 .col-md-8 -->
+<?php
+get_sidebar();
+get_footer();
